ff86542922ecdee332c0451cbc41a84b7373cd9d
Sebastian Madrid <soygenial56@gmail.com>
2022-11-08 23:07:25 -0600

    segundo commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-if('serviceWorker' in navigator){
-    console.log('Puedes usar los service worker en tu navegador');
-    navigator.serviceWorker.register('./sw.js')
-    .then(res=>console.log('serviceWorker cargado correctamente', res))
-    .catch(err=>console.log('Caca', err))
-}else{
-    console.log('No se puede naco prieto esclavo del sistema');
-}
+document.querySelectorAll(".carousel-history").forEach(carousel=>{
+    const items = carousel.querySelectorAll(".carousel__item")
+    items.forEach((item, i)=>{
+        item.innerHTML = `<img src="images/${i+1}.jpg">`
+    })
+    const buttonsHtml = Array.from(items, ()=>{
+        return `<span class="carousel_button"></span>`
+    })
+
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel_nav">
+            ${buttonsHtml.join("")}
+        </div>
+    `)
+
+    const buttons = carousel.querySelectorAll(".carousel_button")
+    console.log(buttons)
+    buttons.forEach((button, i)=>{
+        button.addEventListener("click", ()=>{
+            items.forEach(item => item.classList.remove("carousel_item-selected"))
+            buttons.forEach(button => button.classList.remove("carousel_button-selected"))
+
+            items[i].classList.add("carousel_item-selected")
+            button.classList.add("carousel_button-selected")
+
+        })
+    })
+
+    items[0].classList.add("carousel_item-selected")
+    buttons[0].classList.add("carousel_button-selected")
+})
+
